8a3af62b927a994820b04b7bcbbb49f8ac2471fd
Anathug <anatoletouvron@hotmail.fr>
2020-07-17 20:22:02 +0200

    :sparkles: static css done

--- dist/DEV.e31bb0bc.js
+++ dist/DEV.e31bb0bc.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56407" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64187" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
